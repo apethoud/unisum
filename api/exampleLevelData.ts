a7ceded2d3d2f572e1f610d9955019b98a2c3a9c
Andrew Pethoud <andrewpethoud@gmail.com>
2023-08-31 16:24:38 -0700

    feat(LevelView): add example game grid data and pass it into the game grid

---
+++ api/exampleLevelData.ts
@@ -0,0 +1,119 @@
+const levelData = {
+  id: 1,
+  pack: "Beginner",
+  targetNumber: 8,
+  gridLayout: [
+    [
+      {
+        id: 1,
+        value: null,
+      },
+      {
+        id: 2,
+        value: null,
+      },
+      {
+        id: 3,
+        value: null,
+      },
+      {
+        id: 4,
+        value: null,
+      },
+      {
+        id: 5,
+        value: null,
+      }
+    ],
+    [
+      {
+        id: 6,
+        value: 5,
+      },
+      {
+        id: 7,
+        value: null,
+      },
+      {
+        id: 8,
+        value: null,
+      },
+      {
+        id: 9,
+        value: 14,
+      },
+      {
+        id: 10,
+        value: null,
+      }
+    ],
+    [
+      {
+        id: 11,
+        value: null,
+      },
+      {
+        id: 12,
+        value: null,
+      },
+      {
+        id: 13,
+        value: null,
+      },
+      {
+        id: 14,
+        value: null,
+      },
+      {
+        id: 15,
+        value: null,
+      }
+    ],
+    [
+      {
+        id: 16,
+        value: null,
+      },
+      {
+        id: 17,
+        value: null,
+      },
+      {
+        id: 18,
+        value: null,
+      },
+      {
+        id: 19,
+        value: 17,
+      },
+      {
+        id: 20,
+        value: null,
+      }
+    ],
+    [
+      {
+        id: 21,
+        value: null,
+      },
+      {
+        id: 22,
+        value: null,
+      },
+      {
+        id: 23,
+        value: null,
+      },
+      {
+        id: 24,
+        value: null,
+      },
+      {
+        id: 25,
+        value: null,
+      }
+    ],
+  ]
+}
+
+export default levelData
--- app/level-view/GameGrid.tsx
+++ app/level-view/GameGrid.tsx
@@ -1,22 +1,23 @@
 import { View } from "react-native";
 import Text from "../../reusable-components/Text";
+import levelData from "../../api/exampleLevelData";
 
 const numberOfRows = 5
 const numberOfColumns = 5
 
 export default function GameGrid() {
-  const Cell = () => (
+  const Cell = ({ value }: { value: number }) => (
     <View className="w-12 h-12 flex justify-center items-center border border-slate-300">
-      <Text large>88</Text>
+      <Text large>{value}</Text>
     </View>
   )
 
   const Grid = () => (
     <View className="border border-slate-300">
-      {[...Array(numberOfRows)].map((e, i) => (
+      {levelData.gridLayout.map((row, i) => (
         <View className="flex-row" key={i}>
-          {[...Array(numberOfColumns)].map((e, i) => (
-            <Cell key={i} />
+          {row.map(cell => (
+            <Cell key={cell.id} value={cell.value} />
           ))}
         </View>
       ))}
