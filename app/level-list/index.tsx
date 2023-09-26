ac3756be51a31540336a1bcf4a0ee6ca8cb53ae4
Andrew Pethoud <andrewpethoud@gmail.com>
2023-09-26 16:47:42 -0700

    chore(LevelList): fetch level list by tier id

--- app/level-list/index.tsx
+++ app/level-list/index.tsx
@@ -1,13 +1,40 @@
 import { View } from "react-native";
 import Text from "../../reusable-components/Text";
-import { useLocalSearchParams } from "expo-router";
+import { useLocalSearchParams, useRouter } from "expo-router";
+import { useEffect, useState } from "react";
+import { supabase } from "../../supabaseClient";
 
 export default function LevelList() {
-  const params = useLocalSearchParams()
-  console.log("params: ", params)
+  const [levels, setLevels] = useState(null)
+  
+  const { tier_id } = useLocalSearchParams()
+
+  useEffect(() => {
+    async function getLevels() {
+      let { data, error } = await supabase
+        .from('levels')
+        .select(`*`)
+        .eq('tier_id', tier_id)
+        if (error) {
+          console.log("Error: ", error)
+        }
+      console.log("*** data: ", data)
+      setLevels(data)
+    }
+    getLevels()
+  }, [])
+
+  const navigation = useRouter()
+  
+  // const navigateToLevelList = (tierId) => {
+  //   navigation.push({ pathname: 'level-list', params: { tier_id: tierId } })
+  // }
+
   return (
-    <View>
-      <Text>LevelList</Text>
+    <View className="w-full">
+      {levels && levels.map(level => (
+        <Text key={level.id}>Target Number: {level.target_number}</Text>
+      ))}
     </View>
   )
 }
